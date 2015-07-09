743488fe52590582185233ebe89fde48fd96d1bb
Jerry Wong <jerry@jerrywong.net>
2015-07-09 00:01:32 -0400

    Barebones rendering.

--- iOSJunkMetal/iOSJunkMetal/ViewController.m
+++ iOSJunkMetal/iOSJunkMetal/ViewController.m
@@ -11,24 +11,94 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <MTKViewDelegate>
+@property (nonatomic, readonly) MTKView *metalView;
+@property (nonatomic, readonly) id<MTLDevice> device;
+
+@property (nonatomic) id<MTLBuffer> vertexBuffer;
+@property (nonatomic) id<MTLLibrary> defaultLibrary;
+
+@property (nonatomic) MTLRenderPipelineDescriptor *renderPipeline;
+@property (nonatomic) id<MTLRenderPipelineState> pipelineState;
+
+@property (nonatomic) id<MTLCommandQueue> commandQueue;
 
 @end
 
 @implementation ViewController
 
 - (void)loadView {
-    self.view = [[MTKView alloc] init];
+    MTKView *view = [[MTKView alloc] init];
+    view.clearColor = MTLClearColorMake(0.0, 1.0, 0.0, 1.0);
+    view.preferredFramesPerSecond = 60;
+    view.delegate = self;
+    self.view = view;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setup];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (id<MTLDevice>)device {
+    return [(MTKView *)self.view device];
+}
+
+- (MTKView *)metalView {
+    return (MTKView *)self.view;
+}
+
+#pragma mark - Setup
+
+- (void)setup {
+    id<MTLDevice> device = self.device;
+    float vertexData[] = {
+        0.f, 1.f, 0.f,
+        -1.f, -1.f, 0.f,
+        1.f, -1.f, 0.f };
+    NSUInteger vertexDataSize = sizeof(vertexData) * sizeof(vertexData[0]);
+    self.vertexBuffer = [device newBufferWithBytes:&vertexData length:vertexDataSize options:0];
+
+    self.defaultLibrary = [device newDefaultLibrary];
+    id<MTLFunction> fragShader = [self.defaultLibrary newFunctionWithName:@"basic_fragment"];
+    id<MTLFunction> vertShader = [self.defaultLibrary newFunctionWithName:@"basic_vertex"];
+
+    MTLRenderPipelineDescriptor *pipeline = [[MTLRenderPipelineDescriptor alloc] init];
+    pipeline.vertexFunction = vertShader;
+    pipeline.fragmentFunction = fragShader;
+    pipeline.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm;
+    self.renderPipeline = pipeline;
+
+    NSError *pipelineError = nil;
+    self.pipelineState = [device newRenderPipelineStateWithDescriptor:self.renderPipeline error:&pipelineError];
+    if (pipelineError || !self.pipelineState) {
+        NSLog(@"CRITICAL: Failed to create pipeline state: %@", pipelineError);
+    }
+
+    self.commandQueue = [device newCommandQueue];
+}
+
+#pragma mark - MTKViewDelegate
+
+- (void)drawInView:(nonnull MTKView *)view {
+    MTKView *metalView = self.metalView;
+    MTLRenderPassDescriptor *renderPassDescriptor = metalView.currentRenderPassDescriptor;
+    id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer];
+
+    id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+    if (renderEncoder) {
+        [renderEncoder setRenderPipelineState:self.pipelineState];
+        [renderEncoder setVertexBuffer:self.vertexBuffer offset:0 atIndex:0];
+        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3 instanceCount:1];
+        [renderEncoder endEncoding];
+    }
+
+    [commandBuffer presentDrawable:metalView.currentDrawable];
+    [commandBuffer commit];
+}
+
+- (void)view:(nonnull MTKView *)view willLayoutWithSize:(CGSize)size {
+
 }
 
 @end
